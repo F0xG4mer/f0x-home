014b6f19536fe0003027fb5654fa713efebc286e
F0x <F0xG4mer321go@gmail.com>
2025-08-11 17:57:02 +0200

    Added Cover animation

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,9 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Cover from './components/Cover.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <Cover/>
+  <h1>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
+sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </h1>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/components/Cover.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="image-cover">
+        <img src="../assets/images/f0x-home-top-cover.webp">
+    </div>
+</template>
+
+<style>
+.image-cover {
+    width: calc(100vw - 2.5rem);
+    height: calc(100vh - 2.5rem);
+    display: flex;
+    justify-content: center;
+    margin: 1.25rem;
+    overflow: hidden;
+    border-radius: 32px;
+
+}
+
+.image-cover img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    scale: 130%;
+    object-position: 190px 50px;
+
+}
+
+</style>
