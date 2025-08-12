04b0dbc57ebc57b3d732a5c644c3aa098cf3c361
F0x <F0xG4mer321go@gmail.com>
2025-08-12 17:55:42 +0200

    Animate Card on hover; Made Background gradient darekr

--- src/components/Sections/ProjectSection.vue
+++ src/components/Sections/ProjectSection.vue
@@ -24,7 +24,6 @@ export default {
             }
             return num.toString()
         },
-
         formatDate(date) {
             return new Date(date).toLocaleDateString("en-GB", {
                 day: "numeric",
@@ -32,6 +31,9 @@ export default {
                 year: "numeric"
 
             })
+        },
+        openVideo(id) {
+            window.open(`https://www.youtube.com/watch?v=${id}`, '_blank');
         }
 
     }
@@ -40,17 +42,19 @@ export default {
 
 <template>
     <div class="projects-container" id="projects">
-        <h1>&#8968;Projects &#38; Contributions &#8971;</h1>
+        <h1>&#8968;Projects &#38; Contributions&#8971;</h1>
         <div class="projects-list">
             <div v-for="project in projectsList" :key="project.id">
-                <div class="project-item">
+                <div class="project-item" @click="openVideo(project.id)">
                     <img :src="getThumbnailUrl(project.id)" :alt="project.title">
-                    <div class="overlay">
-                        <span class="channel overlay-bg">{{ project.channel}}</span>
+                    <div class="overlay-1">
+                        <span class="channel overlay-bg">{{ project.channel }}</span>
                         <span class="views overlay-bg">{{ formatNumber(project.views) }} views</span>
                         <span class="upload overlay-bg">{{ formatDate(project.upload) }}</span>
-                        <span class="title overlay-bg">{{ project.title }}</span>
                     </div>
+                    <!-- <div class="overlay-2">
+                        <span class="title overlay-bg">{{ project.title }}</span>
+                    </div> -->
                 </div>
             </div>
         </div>
@@ -93,67 +97,90 @@ export default {
 */
 
 .project-item {
+    cursor: pointer;
     position: relative;
     width: 560px;
     height: 315px;
+    transition: all 500ms;
 
 }
 
+
+.project-item:hover {
+    transform: rotate(1deg) scale(104%) translateY(-10px);
+
+}
 
 .project-item img {
     object-fit: contain;
     width: 100%;
     height: 100%;
     border-radius: 16px;
-
 }
 
-.overlay {
+.overlay-1 {
+    display: block;
     position: absolute;
     top: 0;
     width: 100%;
     height: 100%;
     border-radius: 16px;
     font-size: 20px;
-    transition: all 350ms;
+    z-index: 10;
+}
+
+.overlay-2 {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 16px;
+    font-size: 20px;
+    z-index: 5;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 
 }
 
-.overlay:hover {
-    backdrop-filter: grayscale(1) blur(4px);
+/* .overlay-1:hover {
+    opacity: 0;
 }
+
+.overlay-2:hover {
+    backdrop-filter: grayscale(1) blur(5px);
+} */
 
 .overlay-bg {
     padding: 5px 6px 5px 6px;
     backdrop-filter: grayscale(1) blur(3px) brightness(0.25);
     border-radius: 8px;
+
 }
 
-.overlay > .channel {
+.overlay-1>.channel {
     position: inherit;
     top: 1.25rem;
     left: 1.25rem;
 }
 
-.overlay > .upload {
+.overlay-1>.upload {
     position: inherit;
     bottom: 1.25rem;
     right: 1.25rem;
 }
-.overlay > .views {
+
+.overlay-1>.views {
     position: inherit;
     bottom: 1.25rem;
     left: 1.25rem;
 }
 
-.overlay > .title {
+.overlay-2>.title {
     position: inherit;
     font-size: 24px;
     width: calc(100% - 100px);
-    top: 50%;
     left: calc(50% - (460px / 2));
 }
-
-
-
 </style>
